47cf7e330a49a2fa09153c0dfc1f5a6db37eaf61
TaoBo <1277389320@qq.com>
2026-01-25 13:54:55 +0800

    实现边缘到边缘支持，确保在Android 15及以上版本的兼容性

--- android/app/src/main/kotlin/com/elixir/esports/MainActivity.kt
+++ android/app/src/main/kotlin/com/elixir/esports/MainActivity.kt
@@ -1,6 +1,15 @@
 package com.elixir.esports
 
+import android.os.Build
+import android.os.Bundle
+import androidx.core.view.WindowCompat
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+        // 实现边缘到边缘支持，确保在Android 15及以上版本的兼容性
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+    }
 }
